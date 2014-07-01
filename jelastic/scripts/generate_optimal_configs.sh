c3a31ad37ae2a9b69f613473a9493e225aeac620
Firstname Lastname <your_email@youremail.com>
2014-07-01 17:27:26 +0300

    updated 2x cartridge

---
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SED=$(which sed);
+
+#
+# memory optimizer for smartfox server
+#
+
+SMARTFOX_CONTROL_SCRIPT="${OPENSHIFT_SFS_DIR}/versions/multiversion/SFS2X/config/server.xml";
+
+[ -z "$XMS" ] && { XMS=32; }
+memory_total=`free -m | grep Mem | awk '{print $2}'`;
+[ -z "$XMX" ] && { let XMX=memory_total-35;  }
+
+$SED -i  "s/-Xms[0-9]*M/-Xms${XMS}M/g"  $SMARTFOX_CONTROL_SCRIPT;
+$SED -i  "s/-Xmx[0-9]*M/-Xmx${XMX}M/g"  $SMARTFOX_CONTROL_SCRIPT;
+
